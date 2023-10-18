b5eaf5b69c1b9a5ea559ed74947f300e246f5f5e
kongukjae <51992661+kongukjae@users.noreply.github.com>
2023-10-18 21:04:52 +0900

    모듈로 get 처리 진행한 함수 적용 테스트 완료

--- controllers/app.js
+++ controllers/app.js
@@ -1,7 +1,11 @@
 const http = require('http');
 const fs = require('fs');
+const handleGETRequest = require('./request-handle/handleGETRequest');
 
 const server = http.createServer((req, res) => {
+  if(req.method === "GET") {
+    handleGETRequest(req, res);
+  }
 });
 
 const _PORT = 3000;
