438498c1f897f26dbb63c19afbaaabbea7663972
kongukjae <51992661+kongukjae@users.noreply.github.com>
2023-10-19 21:58:55 +0900

    편안한 관리를 위해 문서 스크립트용 파일, 함수 제작

--- views/index.js
+++ views/index.js
@@ -1,3 +1,17 @@
-// html에 직접적으로 연결되는 파일로 설정
+// * 문서에 직접적으로 연결된 index.js 파일
 
-console.log('hello');
+// * initialize
+const root = document.getElementById('root');
+
+// ******************************************
+
+/**
+ * * render()
+ * * 문서에 렌더링하는 실행함수
+ */
+const render = () => {
+  root.innerHTML = `
+    <h1>Hello World</h1>
+  `;
+};
+render();
