22d6121944db91ebd1d45c9925848d32c5680416
kongukjae <51992661+kongukjae@users.noreply.github.com>
2023-10-24 20:29:41 +0900

    디버깅을 위한 트랜잭션 트래킹

--- controllers/request-handle/handleGETRequest.js
+++ controllers/request-handle/handleGETRequest.js
@@ -2,18 +2,18 @@ const fs = require('fs');
 const path = require('path');
 const filePath = path.join(__dirname, '..', '..', 'views', 'public', 'index.html');
 module.exports = (req, res) => {
-  console.log('GET 요청 발생 2');
-  console.log(req.url, 3);
+  console.log('GET 요청 tracking-2', 'handleGETRequest.js');
   if (req.url === '/') {
     fs.readFile(filePath, 'utf8', (err, data) => {
-      console.log(4);
+      console.log('GET 요청 tracking-3', 'fs.readFile');
       if (err) { // 에러 처리
+        console.log("GET 요청 tracking-4", "fs.readFile 에러");
         console.log(err);
         res.writeHead(500, {'Content-Type': 'text/plain'});
         res.end('server error');
         return;
       }
-      console.log(5);
+      console.log("GET 요청 tracking-5", "정상 작동");
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.end(data);
     });
