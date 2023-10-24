fd0e1dfec29959875a44789f73d38eb20266f97a
kongukjae <51992661+kongukjae@users.noreply.github.com>
2023-10-24 21:42:31 +0900

    기본형태 라우팅, 안티패턴으로 처리 -> 재 조정 예정

--- controllers/request-handle/handleGETRequest.js
+++ controllers/request-handle/handleGETRequest.js
@@ -17,5 +17,33 @@ module.exports = (req, res) => {
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.end(data);
     });
-  }
-} 
+  } else if (req.url === "/views/public/css/index.css") {
+    fs.readFile(path.join(__dirname, '..', '..', 'views', 'public', 'css', 'index.css'), (err, data) => {
+      if (err) {
+        console.log(err);
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('server error');
+        return;
+      }
+      res.writeHead(200, {'Content-Type': 'text/css'});
+      res.end(data);
+    });
+  } else if (req.url === "/views/index.js") {
+    fs.readFile(path.join(__dirname, '..', '..', 'views', 'index.js'), (err, data) => {
+      if (err) {
+        console.log(err);
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('server error');
+        return;
+      }
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.end(data);
+    });
+  } else {  
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('404 not found');
+
+  } // else       
+
+
+} // module.exports 
